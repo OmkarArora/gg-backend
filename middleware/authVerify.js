6817bd1c5b20c52bc3de436957abebbea08c4ea6
Omkar Arora <aroraomkar@gmail.com>
2021-06-14 13:06:43 +0530

    feat: Posts routes

--- middleware/authVerify.js
+++ middleware/authVerify.js
@@ -2,10 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const authVerify = (req, res, next) => {
   const token = req.headers.authorization;
-  console.log("token received", token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userAuth = { userId: decoded.userId, email: decoded.email };
+    req.user = { userId: decoded.userId, email: decoded.email };
     return next();
   } catch (error) {
     return res.status(401).json({
--- routers/post.router.js
+++ routers/post.router.js
@@ -1,7 +1,78 @@
 const express = require("express");
 const router = express.Router();
-
 const { authVerify } = require("../middleware/authVerify");
+const { Post } = require("../models/post.model");
 
 router.use(authVerify);
 
+router.route("/").post(async (req, res) => {
+  try {
+    const post = req.body;
+    const NewPost = new Post(post);
+    const savedPost = await NewPost.save();
+    savedPost.__v = undefined;
+    res.json({ success: true, post: savedPost });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create post",
+      errorMessage: error.message,
+    });
+  }
+});
+
+router.param("postId", async (req, res, next, postId) => {
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(400).json({
+        success: false,
+        message: "Post not found",
+        errorMessage: "Post not found",
+      });
+    }
+    req.post = post;
+    next();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Error while retreiving the post" });
+  }
+});
+
+router
+  .route("/:postId")
+  .get(async (req, res) => {
+    try {
+      const { post } = req;
+      post.__v = undefined;
+      res.json({ success: true, post });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Error while retreiving post",
+        errorMessage: error.message,
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      let { post } = req;
+      post = await post.remove();
+	  post.__v = undefined;
+      res.json({
+        success: true,
+        message: "Post deleted successfully",
+        post,
+        deleted: true,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Error while deleting post",
+        errorMessage: error.message,
+      });
+    }
+  });
+
+module.exports = router;
